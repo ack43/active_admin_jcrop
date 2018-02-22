0d69b8cc88c5557705f8ce20f3948a52feba14d8
Alexander Kiseliev <i43ack@gmail.com>
2018-02-22 14:48:38 +0300

    we prefer paperclip

--- lib/active_admin_jcrop/asset_engine.rb
+++ lib/active_admin_jcrop/asset_engine.rb
@@ -1,5 +1,5 @@
-if Gem::Specification::find_all_by_name('carrierwave').any?
-  require 'active_admin_jcrop/asset_engine/carrier_wave'
-elsif Gem::Specification::find_all_by_name('paperclip').any?
+if Gem::Specification::find_all_by_name('paperclip').any?
   require 'active_admin_jcrop/asset_engine/paperclip'
+elsif Gem::Specification::find_all_by_name('carrier_wave').any?
+  require 'active_admin_jcrop/asset_engine/carrier_wave'
 end
